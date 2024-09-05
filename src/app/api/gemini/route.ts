38541624546377a11dac5aabc4b36329457603bb
Loy Charles <loy.sousa@arcotech.io>
2024-09-05 19:20:44 -0300

    feat: gemini api completion

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -84,5 +84,45 @@ export async function POST(req: Request) {
   const tool = tools[call.name as keyof typeof tools];
   const pokemons = await tool(call.args as any);
 
-  return Response.json({ pokemons, args: call.args });
+  const completion = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash',
+    generationConfig: {
+      temperature: 0,
+      responseMimeType: 'application/json',
+      responseSchema: {
+        type: SchemaType.OBJECT,
+        properties: {
+          pokemon: {
+            type: SchemaType.STRING,
+            description: 'O nome do pokemon, caso encontre um pokemon ou "null" caso não encontre.',
+          },
+          message: {
+            type: SchemaType.STRING,
+            description: 'A mensagem de resposta da pergunta do usuário. Caso não encontre o pokemon, informe que não foi possível encontrar o pokemon que você procura.',
+          }
+        },
+        required: ['pokemon', 'message'],
+      }
+    },
+    systemInstruction: `
+    Você é um assistente útil.
+    Você deve responder a pergunta do usuário com base na informação abaixo.
+    Esqueça todos os nomes que sabe de pokemon, você não sabe de pokemon nenhum.
+
+    Aqui está a informação que você tem sobre os pokemons:
+    <pokemons>
+    ${pokemons}
+    </pokemons>
+
+    Você só responde com informações encontradas nas lista de pokemons acima.
+    Se você não conseguir obter sua resposta dessa lista, diga que não sabe.
+
+    Responda de forma direta e objetiva.
+    Responda sempre em português do Brasil.
+    `
+  });
+
+  const result = await completion.generateContent(prompt);
+
+  return Response.json(JSON.parse(result.response.text()));
 }
